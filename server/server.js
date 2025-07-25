bb862d44e71a3438c7aae4c8754a203304436758
Bibhudutta panda <bibhuduttapanda7@gmail.com>
2025-07-25 11:23:28 +0530

    ...

--- server/server.js
+++ server/server.js
@@ -1,12 +1,17 @@
-import app from './app.js';
+import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config();
+const app= express();
+
+app.use(cors());
+app.use(express.json());
 
 const PORT= process.env.PORT;
 const MONGO_URI=process.env.MONGO_URI;
-console.log("MONGO_URI from env:", MONGO_URI);
+
 
 mongoose.connect(MONGO_URI)
     .then(()=>{
